1312ac5cb791127eca080ecc1f633646bf2095b6
shariq-yousuf <shariqyousuf01@gmail.com>
2025-01-29 16:34:18 +0500

    chore: add main component with quiz context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import SplashScreen from './components/splash-screen'
+import Main from './components/main/main'
+import QuizProvider from './contexts/quiz-provider'
 
 function App() {
-  return <SplashScreen />
+  return (
+    <QuizProvider>
+      <Main />
+    </QuizProvider>
+  )
 }
 
 export default App
---
+++ src/components/main/main.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import { useQuiz } from '../../contexts/quiz-context'
+import { ScreenTypes } from '../../types'
+import SplashScreen from '../splash-screen'
+
+// import QuestionScreen from '../QuestionScreen'
+// import QuizDetailsScreen from '../QuizDetailsScreen'
+// import QuizTopicsScreen from '../QuizTopicsScreen'
+// import ResultScreen from '../ResultScreen'
+
+const Main = () => {
+  const { currentScreen, setCurrentScreen } = useQuiz()
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setCurrentScreen(ScreenTypes.QuizTopicsScreen)
+  //   }, 1000)
+  // }, [setCurrentScreen])
+
+  const screenComponents = {
+    [ScreenTypes.SplashScreen]: <SplashScreen />,
+    // [ScreenTypes.QuizTopicsScreen]: <QuizTopicsScreen />,
+    // [ScreenTypes.QuizDetailsScreen]: <QuizDetailsScreen />,
+    // [ScreenTypes.QuestionScreen]: <QuestionScreen />,
+    // [ScreenTypes.ResultScreen]: <ResultScreen />,
+  }
+
+  const ComponentToRender = screenComponents[currentScreen] || <SplashScreen />
+
+  return <>{ComponentToRender}</>
+}
+
+export default Main
