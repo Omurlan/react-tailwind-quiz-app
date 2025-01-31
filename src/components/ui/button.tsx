f3f33173cc9453857ef77557d4ceb6640ebfe86b
shariq-yousuf <shariqyousuf01@gmail.com>
2025-01-31 17:00:11 +0500

    chore: refactor

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const Button: FC<ButtonTypes> = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${big ? 'w-[180px]' : 'w-[150px]'} ${bold ? 'font-semibold' : 'font-normal'} ${outline ? 'bg-card border-theme border' : 'not-disabled:theme-gradient border-none'} active:shadow-active disabled:bg-disabled-button disabled:[&>svg>path]:fill-dark-grey disabled:text-dark-grey flex h-10 cursor-pointer items-center gap-[10px] rounded px-4 text-lg text-black [-webkit-tap-highlight-color:transparent] [tap-highlight-color:transparent] active:transition-all active:duration-[0.2s] not-disabled:active:scale-[0.98] disabled:transform-[unset] disabled:cursor-not-allowed disabled:shadow-none md:min-h-[50px] md:w-[195px] md:[-webkit-tap-highlight-color:unset] md:[tap-highlight-color:unset] ${iconPosition === 'left' ? 'justify-center' : 'justify-between'}`}
+      className={`${big ? 'w-[180px]' : 'w-[150px]'} ${bold ? 'font-semibold' : 'font-normal'} ${outline ? 'bg-card border-theme border' : 'not-disabled:theme-gradient border-none'} active:shadow-active disabled:bg-disabled-button disabled:[&>svg>path]:fill-dark-grey disabled:text-dark-grey hover:bg-theme flex h-10 cursor-pointer items-center gap-[10px] rounded px-4 text-lg text-black transition-colors duration-300 [-webkit-tap-highlight-color:transparent] [tap-highlight-color:transparent] active:transition-all active:duration-[0.2s] not-disabled:active:scale-[0.98] disabled:transform-[unset] disabled:cursor-not-allowed disabled:shadow-none md:min-h-[50px] md:w-[195px] md:[-webkit-tap-highlight-color:unset] md:[tap-highlight-color:unset] ${iconPosition === 'left' ? 'justify-center' : 'justify-between'}`}
     >
       {icon && iconPosition === 'left' && icon}
       {text}
